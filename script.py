b69c6541ec035811bd51694eb35c77d13ec0b97d
ArcaneFool <37979458+ArcaneFool@users.noreply.github.com>
2018-04-25 19:33:04 +0100

    Update script.py

--- script.py
+++ script.py
@@ -8,7 +8,6 @@ import cassiopeia as cass
 import requests
 
 botprefix = ('<','£' , '=' , '"')
-TOKEN="NDIwODgyNDc2NzQ4MjQyOTQ0.DZFyFw.81RzMdHOt3XBCLLa5MLZ1n2WTV0"
 client = Bot(command_prefix=botprefix)
 
 @client.command(name='8ball',
